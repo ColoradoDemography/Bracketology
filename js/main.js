c29cdf57fe22d9d3d3f60a5b7d3b374c2c4c2344
TBleess <todd.bleess@gmail.com>
2019-04-08 13:19:35 -0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,261 @@
-  cityData = [{"name": "Minneapolis", "pop": 411452}, {"name": "St. Paul", "pop": 300820}, {"name": "Rochester", "pop": 112683}, {"name": "Duluth", "pop": 86066}, {"name": "Bloomington", "pop": 85417}, {"name": "Brooklyn Park", "pop": 79462}, {"name": "Plymouth", "pop": 76258}, {"name": "Maple Grove", "pop": 68362}, {"name": "Woodbury", "pop": 67648}, {"name": "St. Cloud", "pop": 67093}, {"name": "Eagan", "pop": 66102}, {"name": "Eden Prairie", "pop": 63660}, {"name": "Blaine", "pop": 62453}, {"name": "Coon Rapids", "pop": 62342}, {"name": "Burnsville", "pop": 61302}, {"name": "Lakeville", "pop": 61056}, {"name": "Minnetonka", "pop": 52102}, {"name": "Apple Valley", "pop": 51109}, {"name": "Edina", "pop": 50603}, {"name": "St. Louis Park", "pop": 48124}, {"name": "Moorhead", "pop": 41801}, {"name": "Mankato", "pop": 41241}, {"name": "Maplewood", "pop": 40127}, {"name": "Shakopee", "pop": 40024}, {"name": "Richfield", "pop": 35993}, {"name": "Cottage Grove", "pop": 35902}, {"name": "Roseville", "pop": 35624}, {"name": "Inver Grove Hts", "pop": 34976}, {"name": "Andover", "pop": 32257}, {"name": "Brooklyn Ctr", "pop": 30885}, {"name": "Savage", "pop": 30011}, {"name": "Oakdale", "pop": 27972}, {"name": "Fridley", "pop": 27573}, {"name": "Winona", "pop": 27153}, {"name": "Shoreview", "pop": 26432}, {"name": "Ramsey", "pop": 25853}, {"name": "Owatonna", "pop": 25613}, {"name": "Chaska", "pop": 25578}, {"name": "Prior Lake", "pop": 25452}, {"name": "White Bear Lk", "pop": 25411}, {"name": "Chanhassen", "pop": 25108}, {"name": "Austin", "pop": 24882}, {"name": "Champlin", "pop": 24209}, {"name": "Elk River", "pop": 23924}, {"name": "Faribault", "pop": 23577}, {"name": "Rosemount", "pop": 23474}, {"name": "Crystal", "pop": 22910}, {"name": "Hastings", "pop": 22620}, {"name": "Farmington", "pop": 22502}, {"name": "New Brighton", "pop": 22440}, {"name": "Golden Valley", "pop": 21208}, {"name": "Lino Lakes", "pop": 20950}, {"name": "New Hope", "pop": 20909}, {"name": "Northfield", "pop": 20297}, {"name": "South St. Paul", "pop": 20280}, {"name": "Columbia Hts", "pop": 19823}, {"name": "West St. Paul", "pop": 19750}, {"name": "Willmar", "pop": 19608}, {"name": "Forest Lake", "pop": 19406}, {"name": "Stillwater", "pop": 19232}, {"name": "Hopkins", "pop": 18217}, {"name": "Albert Lea", "pop": 17716}, {"name": "Anoka", "pop": 17374}, {"name": "St. Michael", "pop": 17128}]
+  [
+  {
+    "name": "Adams County",
+    "pop": "503,375"
+  },
+  {
+    "name": "Alamosa County",
+    "pop": "16,056"
+  },
+  {
+    "name": "Arapahoe County",
+    "pop": "643,257"
+  },
+  {
+    "name": "Archuleta County",
+    "pop": "13,316"
+  },
+  {
+    "name": "Baca County",
+    "pop": "3,539"
+  },
+  {
+    "name": "Bent County",
+    "pop": "5,866"
+  },
+  {
+    "name": "Boulder County",
+    "pop": "322,854"
+  },
+  {
+    "name": "Broomfield County",
+    "pop": "68,169"
+  },
+  {
+    "name": "Chaffee County",
+    "pop": "19,623"
+  },
+  {
+    "name": "Cheyenne County",
+    "pop": "1,835"
+  },
+  {
+    "name": "Clear Creek County",
+    "pop": "9,625"
+  },
+  {
+    "name": "Conejos County",
+    "pop": "8,117"
+  },
+  {
+    "name": "Costilla County",
+    "pop": "3,771"
+  },
+  {
+    "name": "Crowley County",
+    "pop": "5,749"
+  },
+  {
+    "name": "Custer County",
+    "pop": "4,859"
+  },
+  {
+    "name": "Delta County",
+    "pop": "30,578"
+  },
+  {
+    "name": "Denver County",
+    "pop": "705,651"
+  },
+  {
+    "name": "Dolores County",
+    "pop": "2,040"
+  },
+  {
+    "name": "Douglas County",
+    "pop": "335,635"
+  },
+  {
+    "name": "Eagle County",
+    "pop": "54,662"
+  },
+  {
+    "name": "Elbert County",
+    "pop": "25,594"
+  },
+  {
+    "name": "El Paso County",
+    "pop": "701,283"
+  },
+  {
+    "name": "Fremont County",
+    "pop": "47,521"
+  },
+  {
+    "name": "Garfield County",
+    "pop": "59,167"
+  },
+  {
+    "name": "Gilpin County",
+    "pop": "6,000"
+  },
+  {
+    "name": "Grand County",
+    "pop": "15,297"
+  },
+  {
+    "name": "Gunnison County",
+    "pop": "16,871"
+  },
+  {
+    "name": "Hinsdale County",
+    "pop": 791
+  },
+  {
+    "name": "Huerfano County",
+    "pop": "6,605"
+  },
+  {
+    "name": "Jackson County",
+    "pop": "1,375"
+  },
+  {
+    "name": "Jefferson County",
+    "pop": "575,193"
+  },
+  {
+    "name": "Kiowa County",
+    "pop": "1,364"
+  },
+  {
+    "name": "Kit Carson County",
+    "pop": "7,154"
+  },
+  {
+    "name": "Lake County",
+    "pop": "7,705"
+  },
+  {
+    "name": "La Plata County",
+    "pop": "55,619"
+  },
+  {
+    "name": "Larimer County",
+    "pop": "343,853"
+  },
+  {
+    "name": "Las Animas County",
+    "pop": "14,197"
+  },
+  {
+    "name": "Lincoln County",
+    "pop": "5,526"
+  },
+  {
+    "name": "Logan County",
+    "pop": "21,893"
+  },
+  {
+    "name": "Mesa County",
+    "pop": "151,900"
+  },
+  {
+    "name": "Mineral County",
+    "pop": 752
+  },
+  {
+    "name": "Moffat County",
+    "pop": "13,112"
+  },
+  {
+    "name": "Montezuma County",
+    "pop": "26,074"
+  },
+  {
+    "name": "Montrose County",
+    "pop": "41,763"
+  },
+  {
+    "name": "Morgan County",
+    "pop": "28,075"
+  },
+  {
+    "name": "Otero County",
+    "pop": "18,370"
+  },
+  {
+    "name": "Ouray County",
+    "pop": "4,783"
+  },
+  {
+    "name": "Park County",
+    "pop": "17,892"
+  },
+  {
+    "name": "Phillips County",
+    "pop": "4,275"
+  },
+  {
+    "name": "Pitkin County",
+    "pop": "17,875"
+  },
+  {
+    "name": "Prowers County",
+    "pop": "12,004"
+  },
+  {
+    "name": "Pueblo County",
+    "pop": "165,974"
+  },
+  {
+    "name": "Rio Blanco County",
+    "pop": "6,345"
+  },
+  {
+    "name": "Rio Grande County",
+    "pop": "11,251"
+  },
+  {
+    "name": "Routt County",
+    "pop": "25,178"
+  },
+  {
+    "name": "Saguache County",
+    "pop": "6,631"
+  },
+  {
+    "name": "San Juan County",
+    "pop": 714
+  },
+  {
+    "name": "San Miguel County",
+    "pop": "7,967"
+  },
+  {
+    "name": "Sedgwick County",
+    "pop": "2,314"
+  },
+  {
+    "name": "Summit County",
+    "pop": "30,555"
+  },
+  {
+    "name": "Teller County",
+    "pop": "24,625"
+  },
+  {
+    "name": "Washington County",
+    "pop": "4,921"
+  },
+  {
+    "name": "Weld County",
+    "pop": "304,435"
+  },
+  {
+    "name": "Yuma County",
+    "pop": "10,075"
+  }
+]
 
   function shuffle(a) {
     var j, x, i;
